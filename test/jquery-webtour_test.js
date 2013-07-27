71a77aa24e7eb529abfa07a47bdf4b0bb1d0e591
Helielson <helielson@jusbrasil.com.br>
2013-07-27 17:53:00 -0300

    Starting unit tests #2

--- test/jquery-webtour_test.js
+++ test/jquery-webtour_test.js
@@ -1,27 +1,6 @@
 (function($) {
-  /*
-    ======== A Handy Little QUnit Reference ========
-    http://api.qunitjs.com/
 
-    Test methods:
-      module(name, {[setup][ ,teardown]})
-      test(name, callback)
-      expect(numberOfAssertions)
-      stop(increment)
-      start(decrement)
-    Test assertions:
-      ok(value, [message])
-      equal(actual, expected, [message])
-      notEqual(actual, expected, [message])
-      deepEqual(actual, expected, [message])
-      notDeepEqual(actual, expected, [message])
-      strictEqual(actual, expected, [message])
-      notStrictEqual(actual, expected, [message])
-      throws(block, [expected], [message])
-  */
-
-  module('jQuery#defaultPluginName', {
-    // This will run before each test in this module.
+  module('jQuery#tour', {
     setup: function() {
       this.elems = $('#qunit-fixture').children();
     }
@@ -29,8 +8,7 @@
 
   test('is chainable', function() {
     expect(1);
-    // Not a bad test to run on collection methods.
-    strictEqual(this.elems.defaultPluginName(), this.elems, 'should be chainable');
+    strictEqual(this.elems.tour(), this.elems, 'should be chainable');
   });
 
 }(jQuery));
